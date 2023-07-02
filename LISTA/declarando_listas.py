4ad13478f41abff5c6bd28cb3187ae4e7318d7a9
jovasdevs <jovasdevs@gmail.com>
2023-07-02 14:32:34 -0300

    first commit

---
+++ LISTA/declarando_listas.py
@@ -0,0 +1,14 @@
+frutas = ["banana", "melancia", "jaca"]
+print(frutas)
+
+frutas = []
+print(frutas)
+
+letras = list("estudar")
+print(letras)
+
+numeros = list(range(5))
+print(numeros)
+
+carro = ["Ford", "Maverick v8", 42000, 1975, 2900, "São Paulo", True]
+print(carro)
